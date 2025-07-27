f896d4fcdf0655d1455a3972bf055d862220761c
sunnysavita10 <engsunny3365@gmail.com>
2025-07-27 14:36:16 +0530

    config code is added

---
+++ utils/config_loader.py
@@ -0,0 +1,9 @@
+import yaml
+
+
+def load_config(config_path: str = "config\config.yaml") -> dict:
+    with open(config_path, "r") as file:
+        config=yaml.safe_load(file)
+    return config
+
+load_config("config\config.yaml")
