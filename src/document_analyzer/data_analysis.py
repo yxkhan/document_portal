1ae28804161a82d8d6540ed751243b6aaa4ae5df
sunnysavita10 <engsunny3365@gmail.com>
2025-07-27 15:49:43 +0530

    document analyzer create

---
+++ src/document_analyzer/data_analysis.py
@@ -0,0 +1,19 @@
+import os
+from utils.model_loader import ModelLoader
+from logger.custom_logger import CustomLogger
+from exception.custom_exception import DocumentPortalException
+from model.models import *
+from langchain_core.output_parsers import JsonOutputParser
+from langchain.output_parsers import OutputFixingParser
+
+class DocumentAnalyzer:
+    """
+    Analyzes documents using a pre-trained model.
+    Automatically logs all actions and supports session-based organization.
+    """
+    def __init__(self):
+        pass
+    
+    def analyze_metadata(self):
+        pass
+    
---
+++ src/document_analyzer/data_ingestion.py
@@ -0,0 +1,21 @@
+import os
+import fitz
+import uuid
+from datetime import datetime
+from logger.custom_logger import CustomLogger
+from exception.custom_exception import DocumentPortalException
+
+class DocumentHandler:
+    """
+    Handles PDF saving and reading operations.
+    Automatically logs all actions and supports session-based organization.
+    """
+    def __init__(self):
+        pass
+    
+    def save_pdf(self):
+        pass
+    
+    def read_pdf(self):
+        pass
+    
