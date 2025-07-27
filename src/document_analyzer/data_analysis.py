7f0a30c5662b52460100c6f0bb05c4a635dff543
sunnysavita10 <engsunny3365@gmail.com>
2025-07-27 17:02:01 +0530

    code updated for document analysis

--- src/document_analyzer/data_analysis.py
+++ src/document_analyzer/data_analysis.py
@@ -34,6 +34,26 @@ class DocumentAnalyzer:
         
         
     
-    def analyze_document(self):
-        pass
+    def analyze_document(self, document_text:str)-> dict:
+        """
+        Analyze a document's text and extract structured metadata & summary.
+        """
+        try:
+            chain = self.prompt | self.llm | self.fixing_parser
+            
+            self.log.info("Meta-data analysis chain initialized")
+
+            response = chain.invoke({
+                "format_instructions": self.parser.get_format_instructions(),
+                "document_text": document_text
+            })
+
+            self.log.info("Metadata extraction successful", keys=list(response.keys()))
+            
+            return response
+
+        except Exception as e:
+            self.log.error("Metadata analysis failed", error=str(e))
+            raise DocumentPortalException("Metadata extraction failed") from e
+        
     
