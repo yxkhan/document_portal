50495c22c1eb02aa430cb06f5ec2bce7e8bc029c
sunnysavita10 <engsunny3365@gmail.com>
2025-08-02 15:09:28 +0530

    code skelton have been updated for document comparison

--- model/models.py
+++ model/models.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, RootModel
 from typing import Optional, List, Dict, Any, Union
 
 
@@ -13,3 +13,9 @@ class Metadata(BaseModel):
     PageCount: Union[int, str]  # Can be "Not Available"
     SentimentTone: str
     
+class ChangeFormat(BaseModel):
+    Page: str
+    changes: str
+    
+class SummaryResponse(RootModel[list[ChangeFormat]]):
+    pass
