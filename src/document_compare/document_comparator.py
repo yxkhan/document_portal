c2eeee6c45a8e944cee8f2037ffdaf1ee3bd1f70
sunnysavita10 <engsunny3365@gmail.com>
2025-08-02 15:21:32 +0530

    document comparison file have been updated

--- src/document_compare/document_comparator.py
+++ src/document_compare/document_comparator.py
@@ -11,16 +11,32 @@ from langchain.output_parsers import OutputFixingParser
 
 class DocumentComparatorLLM:
     def __init__(self):
-        pass
-    
+        load_dotenv()
+        self.log = CustomLogger().get_logger(__name__)
+        self.loader = ModelLoader()
+        self.llm = self.loader.load_llm()
+        self.parser = JsonOutputParser(pydantic_object=SummaryResponse)
+        self.fixing_parser = OutputFixingParser.from_llm(parser=self.parser, llm=self.llm)
+        self.prompt = PROPMT_REGISTRY["document_comparison"]
+        self.chain = self.prompt | self.llm | self.parser | self.fixing_parser
+        self.log.info("DocumentComparatorLLM initialized with model and parser.")
+
     def compare_documents(self):
         """
         Compares two documents and returns a structured comparison.
         """
-        pass
+        try:
+            pass
+        except Exception as e:
+            self.log.error(f"Error in compare_documents: {e}")
+            raise DocumentPortalException("An error occurred while comparing documents.",sys)
     
     def _format_response(self):
         """
         Formats the response from the LLM into a structured format.
         """
-        pass
+        try:
+            pass
+        except Exception as e:
+            self.log.error("Error formatting response into DataFrame", error=str(e))
+            raise DocumentPortalException("Error formatting response",sys)
