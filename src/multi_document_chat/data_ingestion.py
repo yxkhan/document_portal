40f96b7ace99fc44209d2db13cc22ff2bf01e520
sunnysavita10 <engsunny3365@gmail.com>
2025-08-09 15:31:05 +0530

    data ingestion code is ready for multiple docs

--- src/multi_document_chat/data_ingestion.py
+++ src/multi_document_chat/data_ingestion.py
@@ -82,7 +82,23 @@ class DocumentIngestor:
             raise DocumentPortalException("Ingestion error in DocumentIngestor", sys)
 
     def _create_retriever(self, documents):
-        try:pass
+        try:
+            splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=300)
+            chunks = splitter.split_documents(documents)
+            self.log.info("Documents split into chunks", total_chunks=len(chunks), session_id=self.session_id)
+            
+            embeddings = self.model_loader.load_embeddings()
+            vectorstore = FAISS.from_documents(documents=chunks, embedding=embeddings)
+            
+            # Save FAISS index under session folder
+            vectorstore.save_local(str(self.session_faiss_dir))
+            self.log.info("FAISS index saved to disk", path=str(self.session_faiss_dir), session_id=self.session_id)
+            
+            retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 5})
+            
+            self.log.info("FAISS retriever created and ready to use", session_id=self.session_id)
+            return retriever
+            
         except Exception as e:
             self.log.error("Failed to create retriever", error=str(e))
             raise DocumentPortalException("Retrieval error in DocumentIngestor", sys)
