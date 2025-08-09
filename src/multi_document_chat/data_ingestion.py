befc455de6eb5558171dfc79a7f1fbab8fc896ca
sunnysavita10 <engsunny3365@gmail.com>
2025-08-09 14:42:33 +0530

    code skeleton is ready for multidoc chat module

---
+++ src/multi_document_chat/data_ingestion.py
@@ -0,0 +1,12 @@
+class DocumentIngestor:
+    def __init__(self):
+        pass
+    
+    def ingest_files(self):
+        pass
+    
+    def _create_retriever(self, documents):
+        pass
+    
+    
+    
---
+++ src/multi_document_chat/retrieval.py
@@ -0,0 +1,22 @@
+class ConversationalRAG:
+    def __init__(self):
+        pass
+    
+    def load_retiever_from_faiss(self):
+        pass
+    
+    def invoke(self):
+        pass
+    
+    def _load_llm(self):
+        pass
+    
+    @staticmethod
+    def _format_docs(docs):
+        pass
+    
+    def _build_lcel_chain(self):
+        pass
+    
+    
+    
