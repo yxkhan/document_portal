a2b5b5f62d5f7966646087ad24dfd5ce2ec05f0f
sunnysavita10 <engsunny3365@gmail.com>
2025-08-23 14:06:51 +0530

    changes updated

--- test.py
+++ test.py
@@ -205,3 +205,39 @@ def test_document_ingestion_and_rag():
         
 if __name__ == "__main__":
     test_document_ingestion_and_rag()
+    
+    
+    
+# Use this code snippet in your app.
+# If you need more information about configurations
+# or implementing the sample code, visit the AWS docs:
+# https://aws.amazon.com/developer/language/python/
+
+# import boto3
+# from botocore.exceptions import ClientError
+
+
+# def get_secret():
+
+#     secret_name = "api_keys"
+#     region_name = "ap-southeast-2"
+
+#     # Create a Secrets Manager client
+#     session = boto3.session.Session()
+#     client = session.client(
+#         service_name='secretsmanager',
+#         region_name=region_name
+#     )
+
+#     try:
+#         get_secret_value_response = client.get_secret_value(
+#             SecretId=secret_name
+#         )
+#     except ClientError as e:
+#         # For a list of exceptions thrown, see
+#         # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+#         raise e
+
+#     secret = get_secret_value_response['SecretString']
+
+#     # Your code goes here.
