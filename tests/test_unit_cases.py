ff96aa64be7242de36e5fa02b5e4e4fd25354cf6
sunnysavita10 <engsunny3365@gmail.com>
2025-08-23 12:56:02 +0530

    changes updated

--- tests/test_unit_cases.py
+++ tests/test_unit_cases.py
@@ -9,4 +9,4 @@ client = TestClient(app)
 def test_home():
     response = client.get("/")
     assert response.status_code == 200
-    assert "Welcome" in response.text
+    assert "Document Portal" in response.text
